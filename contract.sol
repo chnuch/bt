40d31dad87bfad77b31da9f173d5bfd56d7de6ef
chinmayee <159604831+chnuch@users.noreply.github.com>
2025-11-12 21:30:18 +0530

    Create MyContract to manage owner address and balance

---
+++ contract.sol
@@ -0,0 +1,19 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+// Solidity program to retrieve address and balance of owner
+contract MyContract {
+    address private owner;
+
+    constructor() {
+        owner = msg.sender;
+    }
+
+    function getOwner() public view returns (address) {
+        return owner;
+    }
+
+    function getBalance() public view returns (uint256) {
+        return owner.balance;
+    }
+}
